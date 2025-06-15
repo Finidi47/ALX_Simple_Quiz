18341bc26fab164ae029683f5a4d951042d4a72d
Finidi47 <finidisaid@gmail.com>
2025-06-15 14:00:55 +0300

    Check for comparison of the user’s answer with the correct answer

--- quiz.js
+++ quiz.js
@@ -2,21 +2,22 @@ function checkAnswer(){
     const correctAnswer = '4';
     const choices = document.querySelector('input[name="quiz"]:checked');
     const feedback = document.getElementById('feedback');
-    const userAnswer = choices.value;
+
 
     if(choices !== null){
         const userAnswer = choices.value;
+
+            //Compare the User’s Answer with the Correct Answer
+
+        if(userAnswer === correctAnswer){
+            feedback.textContent = "Correct! Well done.";
+        }else {
+            feedback.textContent = "That's incorrect. Try again!";
+        }
     } else{
         feedback.textContent = 'Please select an answer';
     }
 
-    //Compare the User’s Answer with the Correct Answer
-
-    if(userAnswer === correctAnswer){
-        feedback.textContent = "Correct! Well done.";
-    }else if(userAnswer !== correctAnswer){
-        feedback.textContent = "That's incorrect. Try again!";
-    }
 
 }
 
